422d4fd778a4e5e94cddb54f68977236006469ca
Romain niam0r <niamor@mm.st>
2018-02-19 20:47:25 +0000

    added server.js

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const app = express();
 
 app.use(express.static(__dirname));
 
-app.get('/api/posts', (req, res) => {
-  res.send([{ id: 1 }]);
-});
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
